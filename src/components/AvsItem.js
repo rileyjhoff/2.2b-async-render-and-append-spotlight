7f0c2280e89091fc1fb3906128cffab488dd59db
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 16:40:40 -0700

    finished AvsItem component and styling

--- src/components/AvsItem.js
+++ src/components/AvsItem.js
@@ -3,11 +3,12 @@ import { Card } from 'react-bootstrap';
 
 export default function AvsItem({ player }) {
   return (
-    <div className="avs-card">
-      <Card>
-        <Card.Img variant="top" src={player.headshot} />
-        <Card.Title></Card.Title>
-      </Card>
-    </div>
+    <Card className="avs-card">
+      <Card.Img variant="top" src={player.headshot} />
+      <Card.Title>{player.name}</Card.Title>
+      <Card.Subtitle>{'#' + player.number}</Card.Subtitle>
+      <Card.Subtitle>{'Age: ' + player.age}</Card.Subtitle>
+      <Card.Subtitle>{'Position: ' + player.position}</Card.Subtitle>
+    </Card>
   );
 }
