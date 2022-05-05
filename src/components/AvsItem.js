5918dc0836eb67d80056303c6527877a056c170f
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 16:08:56 -0700

    added in supabase URL and KEY. created component files for the first list/items to render & append

---
+++ src/components/AvsItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AvsItem() {
+  return <div>AvsItem</div>;
+}
---
+++ src/components/AvsList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AvsList() {
+  return <div>AvsList</div>;
+}
