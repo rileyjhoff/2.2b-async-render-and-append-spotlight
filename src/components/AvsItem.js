dbb13f9bfd47718b4f8567ddc897372ed713a51b
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 16:30:13 -0700

    started writing basic AvsItem component. props are getting passed through correctly. some intial styling for the AvsItem cards

--- src/components/AvsItem.js
+++ src/components/AvsItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
-export default function AvsItem() {
-  return <div>AvsItem</div>;
+export default function AvsItem({ player }) {
+  return (
+    <div className="avs-card">
+      <Card>
+        <Card.Img variant="top" src={player.headshot} />
+        <Card.Title></Card.Title>
+      </Card>
+    </div>
+  );
 }
--- src/components/AvsList.js
+++ src/components/AvsList.js
@@ -4,9 +4,9 @@ import AvsItem from './AvsItem';
 export default function AvsList({ players }) {
   return (
     <div className="avs-list">
-      {players.map((player, i) => {
-        <AvsItem key={player + i} player={player} />;
-      })}
+      {players.map((player, i) => (
+        <AvsItem key={player + i} player={player} />
+      ))}
     </div>
   );
 }
