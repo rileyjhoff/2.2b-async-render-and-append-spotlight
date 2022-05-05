4516e362353a25b4830eb69b37f3344bee1cfc4d
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 16:50:54 -0700

    finished writing DogItem component and finished up styling. working as expect

--- src/components/DogItem.js
+++ src/components/DogItem.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 export default function DogItem({ dog }) {
-  return <Card className="dog-card"></Card>;
+  return (
+    <Card className="dog-card">
+      <Card.Img variant="top" src={dog.img} />
+      <Card.Title>{dog.breed}</Card.Title>
+      <Card.Subtitle>{'Lifespan: ' + dog.lifespan}</Card.Subtitle>
+      <Card.Subtitle>{'Weight: ' + dog.weight}</Card.Subtitle>
+      <Card.Subtitle>{'Breed Group: ' + dog.group}</Card.Subtitle>
+    </Card>
+  );
 }
