71214aa9500b5ed31511b84b6ec7814e438628b7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 16:16:38 -0700

    wrote fetch function to get first table of data from supabase. wrote a useEffect hook in App to call the fetch function and update state with the response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAvs } from './services/fetch-utils';
 
 function App() {
+  const [avsData, setAvsData] = useState([]);
+
+  useEffect(async () => {
+    const avs = await getAvs();
+    setAvsData(avs);
+  }, []);
+
   return <div className="App"></div>;
 }
 
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,7 @@
-import { client } from './client.js';
+import { checkError, client } from './client.js';
+
+export async function getAvs() {
+  const response = await client.from('avalanche').select('*');
+
+  return checkError(response);
+}
