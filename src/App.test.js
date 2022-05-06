28c1164ff7c72fa1ffc67105d19a97ca174be944
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 17:53:08 -0700

    removed test files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders Supabase import text', () => {
-//   render(<App />);
-//   const textElement = screen.getByText(/Supabase import/i);
-//   expect(textElement).toBeInTheDocument();
-// });
